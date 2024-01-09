ed81f1c600cffe0e7d97fb40fffb4e4d1db44441
Edmund Xin <edmundxin02@gmail.com>
2024-01-09 13:42:44 -0800

    Fix dark mode flicker

--- app/_components/DarkModeToggle.tsx
+++ app/_components/DarkModeToggle.tsx
@@ -1,35 +1,43 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const DarkModeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(
+    typeof window !== "undefined" && localStorage &&
+      localStorage.theme == "dark",
+  );
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsDarkMode(localStorage.theme === 'dark');
-        }
-    }, [])
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsDarkMode(localStorage.theme === "dark");
+    }
+  }, []);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            if (isDarkMode) {
-                document.documentElement.classList.add("dark")
-                localStorage.theme = 'dark'
-            } else {
-                document.documentElement.classList.remove("dark")
-                localStorage.theme = 'light'
-            }
-        }
-    }, [isDarkMode])
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark");
+        localStorage.theme = "dark";
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.theme = "light";
+      }
+    }
+  }, [isDarkMode]);
 
-    return <button className="w-8 h-8 flex items-center justify-center dark:text-stone-200 text-stone-600 rounded-lg dark:hover:text-stone-900 dark:hover:bg-stone-200 hover:text-stone-200 hover:bg-stone-600 transition-colors" onClick={() => {
-        setIsDarkMode(!isDarkMode)
-    }}>
-        <FontAwesomeIcon icon={faMoon} />
+  return (
+    <button
+      className="w-8 h-8 flex items-center justify-center dark:text-stone-200 text-stone-600 rounded-lg dark:hover:text-stone-900 dark:hover:bg-stone-200 hover:text-stone-200 hover:bg-stone-600 transition-colors"
+      onClick={() => {
+        setIsDarkMode(!isDarkMode);
+      }}
+    >
+      <FontAwesomeIcon icon={faMoon} />
     </button>
-}
+  );
+};
 
-export default DarkModeToggle
+export default DarkModeToggle;
